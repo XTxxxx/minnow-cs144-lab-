88cccaf6d673cde2971f56e9d1d55049ab8f3e59
Tianx <xtianx@smail.nju.edu.cn>
2023-12-16 07:02:32 -0600

    checkpoint-2 index-pack completed

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -4,16 +4,22 @@ using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  //1 line
+  return Wrap32{(uint32_t)((n + (uint64_t) zero_point.raw_value_) % (1UL << 32))};
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  //10 line
+  (void) zero_point;
+  uint64_t pow32 = (uint64_t) 1 << 32;
+  uint64_t t = ((uint64_t) (this->raw_value_) + pow32 - zero_point.raw_value_) % pow32;
+  //very important, or will exceed the limit
+  if (t > checkpoint) {
+    return t;
+  }
+  while (t < checkpoint) {
+    t += pow32;
+  }
+  return t - checkpoint > checkpoint + pow32 - t ? t - pow32 : t;
 }
